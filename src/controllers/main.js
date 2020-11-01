ee949fae4b5fca0f21123d8a61860683f773063c
SpahettiCodeDev <spaghetticodedev@gmail.com>
2020-11-01 14:47:58 -0430

    Ad notes UI finished

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,12 +1,39 @@
 const controllers = {
+    // ! Home controller:
     getHome(req, res) {
         res.render('index')
     },
+    // !Login funcionalities:
     signIn(req, res){
         res.send('works 1')
     },
     signUp(req, res){
         res.send('works 2')
+    },
+    // ! Notes functionalities:
+    addNotes(req, res){
+        res.render('NoteView/new-note');
+    },
+    createNotes(req, res){
+        
+    },
+    renderNotes(req, res){
+        res.send('render notes')
+    },
+    editForm(req, res){
+        res.send('render form');
+    },
+    updateNotes(req, res){
+
+    },
+    deleteNote(req, res){
+
     }
+    // ! Todo list functionalities:
+
+    // ! User functionalities:
+
+    // ! links functionalities:
+
 };
 module.exports = controllers;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const route = express.Router();
-const { getHome, signIn, signUp } = require('../controllers/main');
+const { getHome, signIn, signUp, addNotes, createNotes, renderNotes, editForm, updateNotes, deleteNote }
+ = require('../controllers/main');
+
+ /*
+! Home route>
+! signin>
+! signup>
+! add notes form>  
+! send add notes form>
+! get all notes>
+! edit notes form>
+! send edit notes form>
+*/
 
 route.get('/',getHome );
-
 route.get('/signin', signIn);
-route.get('/signup', signUp)
+route.get('/signup', signUp);
+route.get('/notes/add',addNotes);
+route.post('/notes/add', createNotes);
+route.get('/notes', renderNotes);
+route.get('/notes/edit/:id', editForm);
+route.put('/notes/edit/:id', updateNotes);
+route.delete('/notes/delet/:id', deleteNote);
 
 module.exports = route;
